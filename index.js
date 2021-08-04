c7286343e327a078e03764e5737ba26bbc437c97
JacobLinCool <jacoblincool@gmail.com>
2021-08-04 18:03:18 +0800

    Update Template

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 import { main } from "./src/main.js";
 
-const PROGRAM_START_TIME = Date.now();
-
-main().then(() => {
-    console.log(`Execution Time: ${(Date.now() - PROGRAM_START_TIME).toFixed(2)}ms`);
-});
+main();
--- src/main.js
+++ src/main.js
@@ -1,41 +1,21 @@
+import { Router } from "itty-router";
+import { response } from "./response";
+
+const router = Router();
+
+router.all("*", async (request) => {
+    const { query } = request;
+    return response({ data: JSON.stringify(request, null, query.min ? 0 : 2) });
+});
+
 async function main() {
     addEventListener("fetch", (event) => {
-        event.respondWith(handle_request(event));
+        event.respondWith(router.handle(event.request));
     });
 
     addEventListener("scheduled", (event) => {
         event.waitUntil(handle_cron(event));
     });
-}
-
-async function handle_request(event) {
-    let url = new URL(event.request.url);
-    let query = new URLSearchParams(url.search);
-
-    let endpoint = url.pathname.split("/")[1] || "";
-
-    let content = { data: null };
-
-    switch (endpoint) {
-        case "query":
-            content.data = [...query.entries()];
-            break;
-
-        default:
-            content.msg = "Unknown.";
-            break;
-    }
-
-    const response = new Response(JSON.stringify(content, null, query.get("min") ? 0 : 2), {
-        headers: {
-            "Content-Type": "application/json; charset=utf-8",
-            "Cross-Origin-Resource-Policy": "cross-origin",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Credentials": "true",
-            "Cache-Control": "max-age=60, s-maxage=60",
-        },
-    });
-    return response;
 }
 
 export { main };
